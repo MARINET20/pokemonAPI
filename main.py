5f8a0558ec90b3d26b321824c199cc2dda97b46a
MARINET20 <chekanova.lisa-morkovka@mail.ru>
2023-09-25 18:39:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def get_pokemon_names():
+def index():
     BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=151'
     response = requests.get(f"{BASE_URL}")
     data = response.json()
     names = [pokemon['name'] for pokemon in data['results']]
-    return jsonify(names)
-
-
-@app.route('/about')
-def about():
-    return 'Здесь будет информация об авторе сайта.'
+    return render_template('index.html', names=names)
+# def get_pokemon_names():
+#     BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+#     response = requests.get(f"{BASE_URL}")
+#     data = response.json()
+#     names = [pokemon['name'] for pokemon in data['results']]
+#     return jsonify(names)
 
 
 if __name__ == '__main__':
