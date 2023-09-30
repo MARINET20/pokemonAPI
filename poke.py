c47158f6040c3690f6ca876de8b9ddf926bd50b6
MARINET20 <chekanova.lisa-morkovka@mail.ru>
2023-09-30 11:22:51 +0500

    Update poke.py

--- poke.py
+++ poke.py
@@ -5,4 +5,12 @@ response = requests.get(f"{BASE_URL}")
 data = response.json()
 names = [pokemon['name'] for pokemon in data['results']]
 urls = [pokemon['url'] for pokemon in data['results']]
-print(urls)
+print(data)
+
+pokeImages = []
+for pokemon in data['results']:
+    pokeID = pokemon['url'].split('/')[-2]
+    pokeImage = f"<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokeID}.png'>"
+    pokeImages.append(pokeImage)
+
+#print(pokeImages)
