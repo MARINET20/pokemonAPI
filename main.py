af1cd47b9def744bd90b39fb6cec0fdf04bb6552
MARINET20 <chekanova.lisa-morkovka@mail.ru>
2023-09-30 11:22:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask, jsonify, render_template, request
 import requests
+import psycopg2
 
 app = Flask(__name__)
+
+
+def get_db_connection():
+    pass
+
+
+class Pokemon:
+    def __init__(self, name, image):
+        self.name = name
+        self.image = image
 
 
 @app.route('/', methods=['GET'])
@@ -12,12 +23,14 @@ def index():
 
     q = request.args.get('q', '')
     if q:
-        names = [pokemon['name'] for pokemon in data['results'] if q.lower() in pokemon['name'].lower()]
+        pokemon_data = [(pokemon['name'], f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon['url'].split('/')[-2]}.png") for pokemon in data['results'] if q.lower() in pokemon['name'].lower()]
         search_query = q
     else:
-        names = [pokemon['name'] for pokemon in data['results']]
+        pokemon_data = [(pokemon['name'], f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon['url'].split('/')[-2]}.png") for pokemon in data['results']]
 
-    return render_template('index.html', names=names, search_query=q)
+    pokemon_list = [Pokemon(name, image) for name, image in pokemon_data]
+
+    return render_template('index.html', pokemon_list=pokemon_list, search_query=q)
 
 
 if __name__ == '__main__':
