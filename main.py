99c29b75c923e4007ff3f790a926534c866197ca
MARINET20 <chekanova.lisa-morkovka@mail.ru>
2023-10-19 20:39:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -345,9 +345,9 @@ def pokemon(name):
 
 @app.route('/pokemon/save/<name>/<speed>/<hp>/<defense>/<attack>/<weight>',  methods=['GET', 'POST'])
 def save(name, speed, hp, defense, attack, weight):
-    USERNAME = ''
-    PASSWORD = ''
-    HOST = ''
+    USERNAME = 'user'
+    PASSWORD = 'Chekanova2023@'
+    HOST = '192.168.1.7'
 
     ftp = ftplib.FTP(HOST, USERNAME, PASSWORD)
     files = ftp.nlst()
@@ -375,32 +375,7 @@ def save(name, speed, hp, defense, attack, weight):
                            defense=defense, attack=attack, weight=weight)
 
 
-# def save_pokemon_info(name, speed, hp, defense):
-#     USERNAME = 'user'
-#     PASSWORD = 'Chekanova2023@'
-#     HOST = '192.168.1.7'
-#
-#     ftp = ftplib.FTP(HOST, USERNAME, PASSWORD)
-#     files = ftp.nlst()
-#     print(files)
-#
-#     markdown_text = f"# {name}\n\nСкорость: {speed}\n\nЖизнь: {hp}\n\nЗащита: {defense}"
-#     html_text = markdown.markdown(markdown_text)
-#
-#     file_name = f"{name}.md"
-#     current_date = datetime.datetime.now().strftime('%d.%m.%Y')
-#
-#     if current_date in ftp.nlst():
-#         ftp.cwd(current_date)
-#     else:
-#         ftp.mkd(current_date)
-#         ftp.cwd(current_date)
-#
-#     with open(current_date, 'wb') as f:
-#         f.write(markdown_text.encode())
-#     ftp.storbinary('STOR ' + file_name, open(current_date, 'rb'))
-#
-#     ftp.quit()
+
 
 
 if __name__ == '__main__':
